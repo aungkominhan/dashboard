3fd7df35805b89b361a43de02c307a5f5f6e2c8c
aungkominhan <sayakey1210@gamil.com>
2024-11-12 13:48:10 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React = require("react");
+import { Box } from "@mui/material";
+import { Sidebar } from "./components/ui/sidebar";
+
+export default function App(){
+  return (
+    <Box> 
+      
+    </Box>
+  )
+}
